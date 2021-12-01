c5e4cdd836be46e2c5b96fd9af03746d357c6473
RajMazing <caseyraj92@gmail.com>
2021-12-01 03:29:14 +0000

    refactored code from Spinner

---
+++ Spinner2.js
@@ -0,0 +1,131 @@
+
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+ beyBlade = ['\r|', '\r/', '\r-', '\r\\', '\r|', '\r/', '\r-', '\r\\', '\n']
+let start = 200;
+let upAndItsStuck = 100;
+for (let i = 0; i <  beyBlade.length; i++) {
+    start += upAndItsStuck;
+    setTimeout(() => {
+        process.stdout.write( beyBlade[i]);
+    }, start);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sameer
+// const spinner = '\r|   \r/   \r-   \r\\   \r|   \r/   \r-   \r\\   \r'
+
+// let delay = 100;
+
+// for (let i = 0; i < spinner.length; i++) {
+//   delay += 200;
+//   setTimeout(() => process.stdout.write(spinner[i]), delay);
+// }
+
+
+// setTimeout(() => process.stdout.write('\n'), delay);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+
+// }, 100);
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+
+// }, 300);
+
+// setTimeout(() => {
+// process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backlash since its's a special character
+//   process.stdout.write('\r\\    ');
+// }, 700);
+
+
+// // ... fill in the rest yourself...
+
+
+// setTimeout(() => {
+//   // Need to escape the backlash since its's a special character
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   // Need to escape the backlash since its's a special character
+//   process.stdout.write('\r-   ');
+// }, 1300);
+// setTimeout(() => {
+//   // Need to escape the backlash since its's a special character
+//   process.stdout.write('\r\\   ');
+// }, 1500);
+// setTimeout(() => {
+//   // Need to escape the backlash since its's a special character
+//   process.stdout.write('\r|   ');
+// }, 1600);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1900);
